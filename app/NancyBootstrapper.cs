3782471fae0f3a4b26198a5d3b78b87099543fa3
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2013-06-29 12:29:57 +1000

    enabling error tracing

---
+++ app/NancyBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace CsREPL
+{
+	using Nancy;
+	using Nancy.Bootstrapper;
+	using Nancy.TinyIoc;
+
+	public class NancyBootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+		{
+			base.ApplicationStartup(container, pipelines);
+			StaticConfiguration.EnableRequestTracing = true;
+			StaticConfiguration.DisableErrorTraces = false;
+		}
+	}
+}
